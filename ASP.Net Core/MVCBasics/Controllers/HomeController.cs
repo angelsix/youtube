d97260b7aa0629e5dc6637c6f347b94443526f95
Unknown <contact@angelsix.com>
2017-12-21 12:25:05 +0000

    Basic MVC application

---
+++ ASP.Net Core/MVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
