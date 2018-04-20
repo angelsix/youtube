871e589c097f1d97dd914bef5f75312b39b10b07
Luke Malpass <contact@angelsix.com>
2018-04-20 16:33:21 +0100

    Working URLs for template images

--- Email HTML Template/EmailSendTester/Program.cs
+++ Email HTML Template/EmailSendTester/Program.cs
@@ -14,17 +14,21 @@ namespace EmailSendTester
             var username = "noreply@fasetto.com";
             var password = "somepassword";
 
-            var msg = new MailMessage("noreply@fasetto.com", "contact@angelsix.com");
+            var msg = new MailMessage("noreply@fasetto.com", "contact@angelsix.com")
+            {
+                Subject = "Verify Your Account",
+                Body = File.ReadAllText(@"C:\Users\Luke\Desktop\email.txt"),
+                IsBodyHtml = true
+            };
 
-            msg.Subject = "Verify Your Account";
-            msg.Body = File.ReadAllText(@"C:\Users\Luke\Desktop\Email\emailtest.txt");
-            msg.IsBodyHtml = true;
+            var smtpClient = new SmtpClient
+            {
+                Credentials = new NetworkCredential(username, password),
+                Host = "smtp.office365.com",
+                Port = 587,
+                EnableSsl = true
+            };
 
-            var smtpClient = new SmtpClient();
-            smtpClient.Credentials = new NetworkCredential(username, password);
-            smtpClient.Host = "smtp.office365.com";
-            smtpClient.Port = 587;
-            smtpClient.EnableSsl = true;
             smtpClient.Send(msg);
         }
     }
