b10d47925c1e8bcc87e171cd4e631936396f4178
Luke Malpass <contact@angelsix.com>
2022-07-13 15:45:16 +0100

    Start of Avalonia Loudness Meter

---
+++ Avalonia/AvaloniaLoudnessMeter/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaLoudnessMeter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
