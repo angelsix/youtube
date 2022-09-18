a9222dd8cd8e00ddef8c595becc00e5a5a93b4f2
Luke Malpass <contact@angelsix.com>
2022-09-18 17:53:51 +0100

    Animated Popup Control start

---
+++ Avalonia/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Styles/AnimatedPopup.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaLoudnessMeter;
+
+public class AnimatedPopup : ContentControl
+{
+    
+}
--- Avalonia/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
+++ Avalonia/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Media;
 using AvaloniaLoudnessMeter.ViewModels;
