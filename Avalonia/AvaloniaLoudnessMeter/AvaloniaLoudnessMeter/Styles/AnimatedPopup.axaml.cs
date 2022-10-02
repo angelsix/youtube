8f09164b8ff22e7e87b55558d2ef81e944d06072
Luke Malpass <contact@angelsix.com>
2022-10-02 10:09:19 +0100

    Direct properties for Open/Close on animation

--- Avalonia/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Styles/AnimatedPopup.axaml.cs
+++ Avalonia/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Styles/AnimatedPopup.axaml.cs
@@ -1,17 +1,40 @@
 using Avalonia;
 using Avalonia.Animation.Easings;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Threading;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Threading;
 
 namespace AvaloniaLoudnessMeter;
 
-public class AnimatedPopup : ContentControl
+public partial class AnimatedPopup : ContentControl
 {
     #region Private Members
+
+    /// <summary>
+    /// Indicates if this is the first time we are animating
+    /// </summary>
+    private bool mFirstAnimation = true;
+
+    /// <summary>
+    /// Indicates if we have captured the opacity value yet
+    /// </summary>
+    private bool mOpacityCaptured = false;
+    
+    /// <summary>
+    /// Store the controls original Opacity value at startup
+    /// </summary>
+    private double mOriginalOpacity;
+    
+    /// <summary>
+    /// The speed of the animation in FPS
+    /// </summary>
+    private TimeSpan mFrameRate = TimeSpan.FromSeconds(1 / 60.0);
+    
+    // Calculate total ticks that make up the animation time
+    private int mTotalTicks => (int)(_animationTime.TotalSeconds / mFrameRate.TotalSeconds);
 
     /// <summary>
     /// Store the controls desired size
@@ -24,14 +47,88 @@ public class AnimatedPopup : ContentControl
     private bool mAnimating;
 
     /// <summary>
+    /// Keeps track of if we have found the desired 100% width/height auto size
+    /// </summary>
+    private bool mSizeFound;
+
+    /// <summary>
     /// The animation UI timer
     /// </summary>
     private DispatcherTimer mAnimationTimer;
 
     /// <summary>
+    /// The timeout timer to detect when auto-sizing has finished firing
+    /// </summary>
+    private Timer mSizingTimer;
+
+    /// <summary>
     /// The current position in the animation
     /// </summary>
     private int mAnimationCurrentTick;
+    
+    #endregion
+    
+    #region Public Properties
+
+    /// <summary>
+    /// Indicates if the control is currently opened
+    /// </summary>
+    public bool IsOpened => mAnimationCurrentTick >= mTotalTicks;
+
+    #region Open
+    
+    private bool _open;
+
+    public static readonly DirectProperty<AnimatedPopup, bool> OpenProperty = AvaloniaProperty.RegisterDirect<AnimatedPopup, bool>(
+        nameof(Open), o => o.Open, (o, v) => o.Open = v);
+
+    /// <summary>
+    /// Property to set whether the control should be open or closed
+    /// </summary>
+    public bool Open
+    {
+        get => _open;
+        set => SetAndRaise(OpenProperty, ref _open, value);
+    }
+    
+    #endregion
+    
+    #region Animation Time
+
+    private TimeSpan _animationTime = TimeSpan.FromSeconds(3);
+
+    public static readonly DirectProperty<AnimatedPopup, TimeSpan> AnimationTimeProperty = AvaloniaProperty.RegisterDirect<AnimatedPopup, TimeSpan>(
+        nameof(AnimationTime), o => o.AnimationTime, (o, v) => o.AnimationTime = v);
+
+    public TimeSpan AnimationTime
+    {
+        get => _animationTime;
+        set => SetAndRaise(AnimationTimeProperty, ref _animationTime, value);
+    }
+    
+    #endregion
+    
+    #endregion
+    
+    #region Public Commands
+
+    [RelayCommand]
+    public void BeginOpen()
+    {
+        Open = true;
+        
+        // Update animation
+        UpdateAnimation();
+    }
+
+    [RelayCommand]
+    public void BeginClose()
+    {
+        Open = false;
+                
+        // Update animation
+        UpdateAnimation();
+    }
     
     #endregion
 
@@ -42,82 +139,141 @@ public class AnimatedPopup : ContentControl
     /// </summary>
     public AnimatedPopup()
     {
-        // Get a 60 FPS timespan
-        var framerate = TimeSpan.FromSeconds(1 / 60.0);
-
         // Make a new dispatch timer
         mAnimationTimer = new DispatcherTimer
         {
             // Set the timer to run 60 times a second
-            Interval = framerate 
+            Interval = mFrameRate 
         };
-        
-        // Fix for 3 seconds
-        var animationTime = TimeSpan.FromSeconds(1);
 
-        // Calculate total ticks that make up the animation time
-        var totalTicks = (int)(animationTime.TotalSeconds / framerate.TotalSeconds);
+        mSizingTimer = new Timer((t) =>
+        {
+            // If we have already calculated the size...
+            if (mSizeFound)
+                // No longer accept new sizes
+                return;
 
-        // Keep track of current tick
-        mAnimationCurrentTick = 0;
+            // We have now found our desired size
+            mSizeFound = true;
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                // Set the desired size
+                mDesiredSize = DesiredSize - Margin;
+                
+                // Update animation
+                UpdateAnimation();
+            });
+        });
         
         // Callback on every tick
-        mAnimationTimer.Tick += (sender, e) =>
+        mAnimationTimer.Tick += (s, e) => AnimationTick();
+    }
+    
+    #endregion
+    
+    #region Private Methods
+
+    /// <summary>
+    /// Calculate and start any new required animations
+    /// </summary>
+    private void UpdateAnimation()
+    {
+        // Do nothing if we still haven't found our initial size
+        if (!mSizeFound)
+            return;
+        
+        // Start the animation thread again
+        mAnimationTimer.Start();
+    }
+
+    /// <summary>
+    /// Update controls sizes based on the next tick of an animation
+    /// </summary>
+    private void AnimationTick()
+    {
+        // If this is the first call after calculating the desired size...
+        if (mFirstAnimation)
         {
-            // Increment the tick
-            mAnimationCurrentTick++;
-            
-            // Set animating flag
-            mAnimating = true;
+            // Clear the flag
+            mFirstAnimation = false;
 
-            // If we have reached the total ticks...
-            if (mAnimationCurrentTick > totalTicks)
-            {
-                // Stop this animation timer
-                mAnimationTimer.Stop();
-                
-                // Clear animating flag
-                mAnimating = false;
-                
-                // Break out of code
-                return;
-            }
+            // Stop this animation timer
+            mAnimationTimer.Stop();
+            
+            // Reset opacity
+            Opacity = mOriginalOpacity;
+            
+            // Set the final size
+            Width = _open ? mDesiredSize.Width : 0;
+            Height = _open ? mDesiredSize.Height : 0;
 
-            // Get percentage of the way through the current animation
-            var percentageAnimated = (float)mAnimationCurrentTick / totalTicks;
-            
-            // Make an animation easing
-            var easing = new QuadraticEaseIn();
-            
-            // Calculate final width and height
-            var finalWidth = mDesiredSize.Width * easing.Ease(percentageAnimated);
-            var finalHeight = mDesiredSize.Height * easing.Ease(percentageAnimated);
+            // Do on this tick
+            return;
+        }
 
-            // Do our animation
-            Width = finalWidth;
-            Height = finalHeight;
+        // If we have reached the end of our animation...
+        if ((_open && mAnimationCurrentTick >= mTotalTicks) ||
+            (!_open && mAnimationCurrentTick == 0))
+        {
+            // Stop this animation timer
+            mAnimationTimer.Stop();
             
-            Console.WriteLine($"Current tick: {mAnimationCurrentTick}");
-        };
+            // Set the final size
+            Width = _open ? mDesiredSize.Width : 0;
+            Height = _open ? mDesiredSize.Height : 0;
+
+            // Clear animating flag
+            mAnimating = false;
+            
+            // Break out of code
+            return;
+        }
+
+        // Set animating flag
+        mAnimating = true;
+        
+        // Move the tick in the right direction
+        mAnimationCurrentTick += _open ? 1 : -1;
+
+        // Get percentage of the way through the current animation
+        var percentageAnimated = (float)mAnimationCurrentTick / mTotalTicks;
+        
+        // Make an animation easing
+        var easing = new QuadraticEaseIn();
+        
+        // Calculate final width and height
+        var finalWidth = mDesiredSize.Width * easing.Ease(percentageAnimated);
+        var finalHeight = mDesiredSize.Height * easing.Ease(percentageAnimated);
+
+        // Do our animation
+        Width = finalWidth;
+        Height = finalHeight;
+        
+        Console.WriteLine($"Current tick: {mAnimationCurrentTick}");
     }
     
     #endregion
 
     public override void Render(DrawingContext context)
     {
-        // If we are not animating...
-        if (!mAnimating)
+        // If we have not yet found the desired size...
+        if (!mSizeFound)
         {
-            // Set desired size (which includes margin, so remove that from our calculation)
-            mDesiredSize = DesiredSize - Margin;
-            
-            // Reset animation position
-            mAnimationCurrentTick = 0;
-            
-            // Star timer
-            mAnimationTimer.Start();
+            // If we have not yet captured the opacity
+            if (!mOpacityCaptured)
+            {
+                // Set flag to true
+                mOpacityCaptured = true;
+                
+                // Remember original controls opacity
+                mOriginalOpacity = Opacity;
 
-            Console.WriteLine($"Desired size: {mDesiredSize}");
+                // Hide control
+                Opacity = 0;
+            }
+
+            mSizingTimer.Change(100, int.MaxValue);
         }
 
         base.Render(context);
