a0426a22de939a7af844a2a02024d75f0f02ce87
Luke Malpass <contact@angelsix.com>
2025-11-15 09:21:28 +0000

    StreamElectronics Start

---
+++ StreamElectronics/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace StreamElectronics.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ StreamElectronics/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StreamElectronics.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
