a6703b5661726924b973a6e846d36e12f6549c46
Luke Malpass <contact@angelsix.com>
2025-12-09 08:59:41 +0000

    test files for SW

--- Avalonia BatchProcess/Source/BatchProcess3Host/SolidWorks/BatchProcessHost.cs
+++ Avalonia BatchProcess/Source/BatchProcess3Host/SolidWorks/BatchProcessHost.cs
@@ -8,10 +8,10 @@ public class BatchProcessHost
 {
     public string GetSolidWorksVersion() => "SolidWorks 2025 SP1.2";
 
-    public SolidWorksFileDetails GetActiveFile() => new("C:\\Users\\conta\\Desktop\\SW Test Files\\Assem1.SLDASM");
+    public SolidWorksFileDetails GetActiveFile() => new("SolidWorks\\Test Files\\Assem1.SLDASM");
 
     public List<SolidWorksFileDetails> GetActiveFileReferences() => new List<SolidWorksFileDetails>(
-        System.IO.Directory.GetFiles("C:\\Users\\conta\\Desktop\\SW Test Files", 
+        System.IO.Directory.GetFiles("SolidWorks\\Test Files", 
                 "*.*", System.IO.SearchOption.TopDirectoryOnly)
             .Select(f => new SolidWorksFileDetails(f)));
 }
